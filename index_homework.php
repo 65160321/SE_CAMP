148b630f1ac70effc784250521063859bd2cf172
65160321 <65160321@go.buu.ac.th>
2023-12-26 23:44:42 +0700

    Update index_homework.php

--- index_homework.php
+++ index_homework.php
@@ -1,31 +1,29 @@
 <html>
+
     <head>
         <meta charset= "utf-8">
-    </head>
+        <style>
+            
+        </style>
+        <link rel="stylesheet" href="styleHW3.css">    
+          
 
+    </head>
+ 
     <body>
         <center>
-        <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-
-    <table>
-        <tbody>
-            <tr>
+        <?php $multi_x = 25; ?>
+        <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+        <div class="box">
+            <div>
                 <?php
-                    $b = 24;
-                        for( $i=1; $i<=24; $i++ ) {
-                        echo "{$b} x {$i} = ".($b*$i)."<br/>";
+                    for( $i=1; $i<=24; $i++ ) {
+                    echo "{$multi_x} x {$i} = ".($multi_x*$i)."<br/>";
                     }
                 ?>
-            </tr>
-        </tbody>
-    </table>
-    <style>
-        table {
-            width: 150px;
-            height: 265px;
-        }
-    </style>
+            </div>          
+        </div>
         </center>
+        
     </body>
 </html>
