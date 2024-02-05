4887210aa34399e46f6493d1501c91c7bb4ffe38
65160321 <65160321@go.buu.ac.th>
2024-02-05 17:16:21 +0700

    change

--- example-app/app/Models/M_titles.php
+++ example-app/app/Models/M_titles.php
@@ -10,8 +10,7 @@ class M_titles extends Model
     use HasFactory;
 
     protected $table = 'titles';
-    protected $primarykey = 'tit_id';
+    protected $primaryKey = 'tit_id';
     public $incrementing = true;
     public $timestamps = false;
-
 }
